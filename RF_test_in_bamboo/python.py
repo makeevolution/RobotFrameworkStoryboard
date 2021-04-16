fa108354ae36c184b72b18edfcbcf646c94377f5
makeevolution <aldo_hasibuan@yahoo.com>
2021-04-16 17:04:02 +0200

    customize assertion error message

--- RF_test_in_bamboo/python.py
+++ RF_test_in_bamboo/python.py
@@ -3,5 +3,8 @@ from robot.api.deco import keyword
 class python(object): #class name must be the same as python name
     @keyword('the value of ${key} must equal ${value}')
     def check_value(self,key,value):
-        assert key==value
+        try:
+            assert key==value
+        except AssertionError as e:
+            e.args = "test fails, {} is not equal to {}".format(key,value)
 
